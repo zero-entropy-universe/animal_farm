9c6fa063e8618849eb311a994abcd52f89cef8c2
shaochenren <46946162+shaochenren@users.noreply.github.com>
2021-06-10 19:44:03 -0700

    Update settings.py

--- af_proj/af_proj/settings.py
+++ af_proj/af_proj/settings.py
@@ -67,6 +67,22 @@ TEMPLATES = [
     },
 ]
 
+APP_test = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'af_proj.wsgi.application'
 
 
@@ -86,16 +102,17 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        '
+        ': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'PASSWORD': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'PASSWORD': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
