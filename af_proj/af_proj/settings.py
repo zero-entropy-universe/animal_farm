79286d35f06a7c9080daee2c13cd23dc59d38dc1
shaochenren <46946162+shaochenren@users.noreply.github.com>
2021-06-10 19:45:39 -0700

    Update settings.py

--- af_proj/af_proj/settings.py
+++ af_proj/af_proj/settings.py
@@ -116,7 +116,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+PASSWORD_VALID = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
